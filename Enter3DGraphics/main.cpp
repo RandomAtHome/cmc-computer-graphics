141e496978037e6974a76dfaf71719f543fcd7c9
Chukharev Fedor <randomfedor@gmail.com>
2018-12-07 13:08:42 +0300

    Create new project for testing

---
+++ Enter3DGraphics/main.cpp
@@ -0,0 +1,72 @@
+#include "Shader.h"
+
+#include "GL/glew.h"
+#include "GL/freeglut.h"
+#include "GLFW/glfw3.h"
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+#include <soil.h>
+#include <iostream>
+
+// GLEW VERSION IS 4.0
+
+void key_callback(GLFWwindow* window, int key, int scancode, int action, int mode)
+{
+    // Когда пользователь нажимает ESC, мы устанавливаем свойство WindowShouldClose в true, 
+    // и приложение после этого закроется
+    if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS) {
+        glfwSetWindowShouldClose(window, GL_TRUE);
+    }
+}
+
+int main()
+{
+    //Инициализация GLFW
+    glfwInit();
+    //Настройка GLFW
+    //Задается минимальная требуемая версия OpenGL. 
+    //Мажорная 
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    //Минорная
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    //Установка профайла для которого создается контекст
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    //Выключение возможности изменения размера окна
+    glfwWindowHint(GLFW_RESIZABLE, GL_FALSE);
+    GLFWwindow* window = glfwCreateWindow(800, 600, "Learn3DOpenGL", nullptr, nullptr);
+    if (window == nullptr)
+    {
+        std::cout << "Failed to create GLFW window" << std::endl;
+        glfwTerminate();
+        return -1;
+    }
+    glfwMakeContextCurrent(window);
+    glewExperimental = GL_TRUE;
+    if (glewInit() != GLEW_OK)
+    {
+        std::cout << "Failed to initialize GLEW" << std::endl;
+        return -1;
+    }
+    int width, height;
+    glfwGetFramebufferSize(window, &width, &height);
+    glViewport(0, 0, width, height);
+    glfwSetKeyCallback(window, key_callback);
+
+
+    // Game loop
+    while (!glfwWindowShouldClose(window))
+    {
+        // Check if any events have been activiated (key pressed, mouse moved etc.) and call corresponding response functions
+        glfwPollEvents();
+        // Clear the colorbuffer
+        glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        // Swap the screen buffers
+        glfwSwapBuffers(window);
+    }
+    // Terminate GLFW, clearing any resources allocated by GLFW.
+    glfwTerminate();
+    return 0;
+}
