b46f15713c9bd63393fe113d9cb4311b47a79b4d
Chukharev Fedor <randomfedor@gmail.com>
2018-12-07 10:43:22 +0300

    Add Shader OOP wrapper Test rotations etc, study basics of vertex manipulation

--- ComputerGraphics/main.cpp
+++ ComputerGraphics/main.cpp
@@ -1,41 +1,127 @@
-#include "GL\glew.h"
-#include "GL\freeglut.h"
+#include "GL/glew.h"
+#include "GL/freeglut.h"
+#include "GLFW/glfw3.h"
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+#include "Shader.h"
 #include <iostream>
 
 // GLEW VERSION IS 4.0
 void renderScene(void)
 {
-
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
     glClearColor(0.0, 0.3, 0.3, 1.0);
 
     glutSwapBuffers();
 }
 
-int main(int argc, char **argv)
+void key_callback(GLFWwindow* window, int key, int scancode, int action, int mode)
 {
-
-    glutInit(&argc, argv);
-    glutInitDisplayMode(GLUT_DEPTH | GLUT_DOUBLE | GLUT_RGBA);
-    glutInitWindowPosition(500, 500);
-    glutInitWindowSize(800, 600);
-    glutCreateWindow("OpenGL First Window");
-
-    glewInit();
-    if (glewIsSupported("GL_VERSION_4_0"))
-    {
-        std::cout << " GLEW Version is 4.0\n ";
+    // Когда пользователь нажимает ESC, мы устанавливаем свойство WindowShouldClose в true, 
+    // и приложение после этого закроется
+    if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS) {
+        glfwSetWindowShouldClose(window, GL_TRUE);
     }
-    else
+}
+
+int main()
+{
+    //Инициализация GLFW
+    glfwInit();
+    //Настройка GLFW
+    //Задается минимальная требуемая версия OpenGL. 
+    //Мажорная 
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    //Минорная
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    //Установка профайла для которого создается контекст
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    //Выключение возможности изменения размера окна
+    glfwWindowHint(GLFW_RESIZABLE, GL_FALSE);
+    GLFWwindow* window = glfwCreateWindow(800, 600, "LearnOpenGL", nullptr, nullptr);
+    if (window == nullptr)
     {
-        std::cout << "GLEW 4.0 not supported\n ";
+        std::cout << "Failed to create GLFW window" << std::endl;
+        glfwTerminate();
+        return -1;
     }
+    glfwMakeContextCurrent(window);
+    glewExperimental = GL_TRUE;
+    if (glewInit() != GLEW_OK)
+    {
+        std::cout << "Failed to initialize GLEW" << std::endl;
+        return -1;
+    }
+    int width, height;
+    glfwGetFramebufferSize(window, &width, &height);
+    glViewport(0, 0, width, height);
+    glfwSetKeyCallback(window, key_callback);
 
-    glEnable(GL_DEPTH_TEST);
-    // register callbacks
-    glutDisplayFunc(renderScene);
+    // Build and compile our shader program
+    Shader ourShader("Shaders/simple_shader.vert", "Shaders/colors_shader.frag");
 
-    glutMainLoop();
+    GLfloat vertices[] = {
+        // Позиции         // Цвета
+         0.5f, -0.5f, 0.0f,  1.0f, 0.0f, 0.0f,   // Нижний правый угол
+        -0.5f, -0.5f, 0.0f,  0.0f, 1.0f, 0.0f,   // Нижний левый угол
+         0.0f,  0.5f, 0.0f,  0.0f, 0.0f, 1.0f    // Верхний угол
+    };
+    
+    GLuint VBO, VAO;
+    glGenVertexArrays(1, &VAO);
+    glGenBuffers(1, &VBO);
+    // Bind the Vertex Array Object first, then bind and set vertex buffer(s) and attribute pointer(s).
+    glBindVertexArray(VAO);
 
-    return 0;
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+    // Атрибут с координатами
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(GLfloat), (GLvoid*)0);
+    glEnableVertexAttribArray(0);
+
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(GLfloat), (GLvoid*)(3 * sizeof(GLfloat)));
+    glEnableVertexAttribArray(1);
+
+    glBindVertexArray(0); // Unbind VAO
+
+    glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
+    // Game loop
+    while (!glfwWindowShouldClose(window))
+    {
+        // Check if any events have been activiated (key pressed, mouse moved etc.) and call corresponding response functions
+        glfwPollEvents();
+        // Clear the colorbuffer
+        glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        ourShader.Use();
+
+        GLfloat timeValue = glfwGetTime();
+        GLfloat greenValue = (sin(timeValue) / 2) + 0.5;
+        GLint vertexColorLocation = glGetUniformLocation(ourShader.Program, "vertexColor");
+        glUniform4f(vertexColorLocation, 0.0f, greenValue, 0.0f, 0.0f);
+
+        GLint transformLoc  = glGetUniformLocation(ourShader.Program, "transform");
+        glm::mat4 trans(1.f);
+        trans = glm::rotate(trans, (GLfloat)timeValue* 2.f, glm::vec3(1.0f, 0.0f, 0.f));
+        glUniformMatrix4fv(transformLoc, 1, GL_FALSE, glm::value_ptr(trans));
+        // Render
+   
+
+        // Draw the triangle
+        glBindVertexArray(VAO);
+        glDrawArrays(GL_TRIANGLES, 0, 3);
+        glBindVertexArray(0);
+
+        // Swap the screen buffers
+        glfwSwapBuffers(window);
+    }
+    // Properly de-allocate all resources once they've outlived their purpose
+    glDeleteVertexArrays(1, &VAO);
+    glDeleteBuffers(1, &VBO);
+    // Terminate GLFW, clearing any resources allocated by GLFW.
+    glfwTerminate();
+   return 0;
 }
