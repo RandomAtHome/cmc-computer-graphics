76b4bdfaec5ab1bec6fadcb01fbcc9c2a697c1d6
Chukharev Fedor <randomfedor@gmail.com>
2018-12-16 16:36:13 +0300

    Implement my first TextureMap

--- FirstSceneWithLightning/Shaders/shader.frag
+++ FirstSceneWithLightning/Shaders/shader.frag
@@ -1,47 +1,44 @@
 #version 330 core
-struct Material {
-	vec3 ambient; //ambient, object color
-	vec3 diffuse; //diffuse color
-	vec3 specular; //blick color
-	float shininess; //radius of a blick
-};
-uniform Material material;
+out vec4 FragColor;
+
+
+uniform sampler2D m_diffuse;
+uniform sampler2D m_specular;
+uniform float m_shininess;
 
 struct Light {
-	vec3 position; //global world coordinates
-	//intensity
-	vec3 ambient;
-	vec3 diffuse;
-	vec3 specular;
+    vec3 position;
+
+    vec3 ambient;
+    vec3 diffuse;
+    vec3 specular;
 };
-uniform Light light;
 
-in vec3 Normal;
-in vec3 FragPos;
-
-out vec4 color;
-
-uniform vec3 lightColor;
-uniform vec3 lightPos;
+in vec3 FragPos;  
+in vec3 Normal;  
+in vec2 TexCoords;
+  
 uniform vec3 viewPos;
+uniform Light light;
 
 void main()
 {
-	//ambient
-    vec3 ambient = light.ambient * material.ambient;
-
-	//diffuse
+    // ambient
+    vec3 ambient = light.ambient * texture(m_diffuse, TexCoords).rgb;
+  	
+    // diffuse 
     vec3 norm = normalize(Normal);
     vec3 lightDir = normalize(light.position - FragPos);
     float diff = max(dot(norm, lightDir), 0.0);
-    vec3 diffuse = light.diffuse * (diff * material.diffuse);
+    vec3 diffuse = light.diffuse * diff * texture(m_diffuse, TexCoords).rgb;
     
-	//specular
+    // specular
     vec3 viewDir = normalize(viewPos - FragPos);
-    vec3 reflectDir = reflect(-lightDir, norm);
-    float spec = pow(max(dot(viewDir, reflectDir), 0.0), material.shininess);
-    vec3 specular = light.specular * (spec * material.specular);
-
+    vec3 reflectDir = reflect(-lightDir, norm);  
+    float spec = pow(max(dot(viewDir, reflectDir), 0.0), m_shininess);
+    vec3 specular = light.specular * spec * texture(m_specular, TexCoords).rgb;
+        
     vec3 result = ambient + diffuse + specular;
-    color = vec4(result, 1.0f);
-}
+    FragColor = vec4(result, 1.0);
+} 
+
--- FirstSceneWithLightning/main.cpp
+++ FirstSceneWithLightning/main.cpp
@@ -51,6 +51,43 @@ void processCameraMovement(Camera &camera) {
     }
 }
 
+unsigned int loadTexture(char const * path)
+{
+    unsigned int textureID;
+    glGenTextures(1, &textureID);
+
+    int width, height, nrComponents;
+    unsigned char *data = SOIL_load_image(path, &width, &height, &nrComponents, 0);
+    if (data)
+    {
+        GLenum format;
+        if (nrComponents == 1)
+            format = GL_RED;
+        else if (nrComponents == 3)
+            format = GL_RGB;
+        else if (nrComponents == 4)
+            format = GL_RGBA;
+
+        glBindTexture(GL_TEXTURE_2D, textureID);
+        glTexImage2D(GL_TEXTURE_2D, 0, format, width, height, 0, format, GL_UNSIGNED_BYTE, data);
+        glGenerateMipmap(GL_TEXTURE_2D);
+
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+
+        SOIL_free_image_data(data);
+    }
+    else
+    {
+        std::cout << "Texture failed to load at path: " << path << std::endl;
+        SOIL_free_image_data(data);
+    }
+
+    return textureID;
+}
+
 int main()
 {
     glfwInit();
@@ -98,36 +135,43 @@ int main()
     GLuint VBO, VAO, lightVAO;
 
     glGenBuffers(1, &VBO);
+    
     glGenVertexArrays(1, &VAO);
     glBindVertexArray(VAO);
-
     glBindBuffer(GL_ARRAY_BUFFER, VBO);
     glBufferData(GL_ARRAY_BUFFER, sizeof(cube_vertices), cube_vertices, GL_STATIC_DRAW);
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(GLfloat), (GLvoid*)0);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(GLfloat), (GLvoid*)0);
     glEnableVertexAttribArray(0);
-    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(GLfloat), (GLvoid*)(3 * sizeof(GLfloat)));
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(GLfloat), (GLvoid*)(3 * sizeof(GLfloat)));
     glEnableVertexAttribArray(1);
+    glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 8 * sizeof(GLfloat), (GLvoid*)(6 * sizeof(GLfloat)));
+    glEnableVertexAttribArray(2);
     glBindVertexArray(0);
 
     glGenVertexArrays(1, &lightVAO);
     glBindVertexArray(lightVAO);
     glBindBuffer(GL_ARRAY_BUFFER, VBO);
     glBufferData(GL_ARRAY_BUFFER, sizeof(cube_vertices), cube_vertices, GL_STATIC_DRAW);
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(GLfloat), (GLvoid*)0);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(GLfloat), (GLvoid*)0);
     glEnableVertexAttribArray(0);
-    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(GLfloat), (GLvoid*)(3 * sizeof(GLfloat)));
-    glEnableVertexAttribArray(1);
     glBindVertexArray(0);
 
     //////////////////////////////////Pre-loop configs
     Camera mainCamera(glm::vec3(0.0f, 0.0f, 3.0f));
     Shader ourShader("Shaders/shader.vert", "Shaders/shader.frag");
     Shader lightingShader("Shaders/shader.vert", "Shaders/light_cube.frag");
+   
+    unsigned int diffuseMap = loadTexture("Textures/container2.png");
+    unsigned int specularMap = loadTexture("Textures/container2_specular.png");
+
+    ourShader.Use();
+    ourShader.setInt("m_diffuse", 0);
+    ourShader.setInt("m_specular", 1);
+
     glm::vec3 lighting_position(1.f, 1.5f, -1.f);
     glm::vec3 box_color(1.0f, 0.5f, 0.31f);
      //isn't obligatory to call in game loop
-    
-    glm::mat4 projection = glm::perspective(glm::radians(45.0f), (GLfloat)screenWidth / screenHeight, 0.1f, 100.0f);
+
     // Game loop
     while (!glfwWindowShouldClose(window))
     {
@@ -147,9 +191,11 @@ int main()
         glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
+        glm::mat4 projection = glm::perspective(glm::radians(mainCamera.Zoom), (GLfloat)screenWidth / screenHeight, 0.1f, 100.0f);
+
         glBindVertexArray(lightVAO);
-        glm::mat4 rotation = glm::rotate(glm::mat4(1.0f), (GLfloat)glm::radians(0.01f), glm::vec3(0.f, 0.f, 1.f));
-        rotation = glm::rotate(rotation, (GLfloat)glm::radians(0.02f*glm::sin(glfwGetTime())), glm::vec3(1.f, 0.f, 0.f));
+        glm::mat4 rotation = glm::rotate(glm::mat4(1.0f), (GLfloat)glm::radians(0.05f), glm::vec3(0.f, 0.f, 1.f));
+        rotation = glm::rotate(rotation, (GLfloat)glm::radians(0.05f*glm::sin(glfwGetTime())), glm::vec3(1.f, 0.f, 0.f));
         lighting_position = rotation * glm::vec4(lighting_position, 1.f);
         lightingShader.Use();
         lightingShader.setMat4("view", mainCamera.GetViewMatrix());
@@ -158,38 +204,34 @@ int main()
         glDrawArrays(GL_TRIANGLES, 0, 36);
         glBindVertexArray(0);
 
+
+
         glBindVertexArray(VAO);
         ourShader.Use();
-
-        ourShader.setVec3("material.ambient", 1.0f, 0.5f, 0.31f);
-        ourShader.setVec3("material.diffuse", 1.0f, 0.5f, 0.31f);
-        ourShader.setVec3("material.specular", 0.5f, 0.5f, 0.5f);
-        ourShader.setFloat("material.shininess", 32.f);
+        ourShader.setFloat("m_shininess", 64.f);
 
         // light properties
-        glm::vec3 lightColor;
-        lightColor.x = sin(glfwGetTime() * 2.0f);
-        lightColor.y = sin(glfwGetTime() * 0.7f);
-        lightColor.z = sin(glfwGetTime() * 1.3f);
-        glm::vec3 diffuseColor = lightColor * glm::vec3(0.5f); // decrease the influence
-        glm::vec3 ambientColor = diffuseColor * glm::vec3(0.2f); // low influence
-        ourShader.setVec3("light.ambient", ambientColor);
-        ourShader.setVec3("light.diffuse", diffuseColor);
+        ourShader.setVec3("light.ambient", 0.2f, 0.2f, 0.2f);
+        ourShader.setVec3("light.diffuse", 0.5f, 0.5f, 0.5f);
         ourShader.setVec3("light.specular", 1.0f, 1.0f, 1.0f);
         ourShader.setVec3("light.position", lighting_position);
         ourShader.setVec3("viewPos", mainCamera.Position);
         
         ourShader.setMat4("view", mainCamera.GetViewMatrix());
         ourShader.setMat4("projection", projection);
- 
-        GLint modelLoc = glGetUniformLocation(ourShader.ID, "model");
+        
+        // bind diffuse map
+        glActiveTexture(GL_TEXTURE0);
+        glBindTexture(GL_TEXTURE_2D, diffuseMap);
+        // bind specular map
+        glActiveTexture(GL_TEXTURE1);
+        glBindTexture(GL_TEXTURE_2D, specularMap);
+
         for (GLuint i = 0; i < 1; i++)
-        {
-            glm::mat4 model(1.f);
-            model = glm::translate(model, cubePositions[i]);
-            GLfloat angle = glm::radians(20.0f) * i;
-            model = glm::rotate(model, angle, glm::vec3(1.0f, 0.3f, 0.5f));
-            ourShader.setMat4("model", model);
+        { 
+            ourShader.setMat4("model", glm::rotate(
+                glm::translate(glm::mat4(1.f), cubePositions[i]), glm::radians(20.0f) * i, glm::vec3(1.0f, 0.3f, 0.5f)
+            ));
             glDrawArrays(GL_TRIANGLES, 0, 36);
         }
         glBindVertexArray(0);
